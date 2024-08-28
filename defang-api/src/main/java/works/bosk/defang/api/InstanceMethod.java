b41f05074bcdafa1aa380f90975c172f6d82387f
Patrick Doyle <patrick.doyle@elastic.co>
2024-08-28 14:51:23 -0400

    InstanceMethod javadocs

--- defang-api/src/main/java/works/bosk/defang/api/InstanceMethod.java
+++ defang-api/src/main/java/works/bosk/defang/api/InstanceMethod.java
@@ -6,6 +6,13 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Indicates that a non-static method should be instrumented to check
+ * for the given {@link Entitlement}.
+ * The annotated method should mimic the actual target method to be instrumented,
+ * with the same name and return type, and the same parameters plus an additional
+ * parameter at the start whose type is the owning class of the target method.
+ */
 @Target(METHOD)
 @Retention(RUNTIME)
 public @interface InstanceMethod {
