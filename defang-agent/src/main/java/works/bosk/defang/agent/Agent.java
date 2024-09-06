d766ecab1ae5288ae3269da27841083ff65dcf3d
Patrick Doyle <patrick.doyle@elastic.co>
2024-09-06 10:56:41 -0400

    Use redefineClasses instead of retransformClasses

--- defang-agent/src/main/java/works/bosk/defang/agent/Agent.java
+++ defang-agent/src/main/java/works/bosk/defang/agent/Agent.java
@@ -8,15 +8,17 @@ import works.bosk.defang.instrumentation.Instrumenter;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
 import java.util.jar.JarFile;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toSet;
 import static works.bosk.defang.runtime.Config.CONFIG_CLASSES;
 
 public class Agent {
-    public static void premain(String agentArgs, Instrumentation inst) throws UnmodifiableClassException, IOException {
+    public static void premain(String agentArgs, Instrumentation inst) throws UnmodifiableClassException, IOException, ClassNotFoundException {
         LOGGER.trace("Starting premain");
         var jarsString = System.getProperty("defang.runtimeJars");
         if (jarsString != null) {
@@ -27,11 +29,22 @@ public class Agent {
         var scanResults = ConfigScanner.scanConfig(CONFIG_CLASSES);
         inst.addTransformer(new Transformer(
             new Instrumenter("", scanResults.instrumentationMethods()),
-            scanResults.classesToInstrument().stream().map(Type::getInternalName).collect(toSet())),
-            true);
-        LOGGER.trace("Starting retransformClasses");
-        inst.retransformClasses(scanResults.classesToInstrument().toArray(new Class[0]));
+            scanResults.classesToInstrument().stream().map(Type::getInternalName).collect(toSet())));
+        LOGGER.trace("Starting redefineClasses");
+        var classesToInstrument = scanResults.classesToInstrument().toArray(new Class[0]);
+        ClassDefinition[] classDefinitions = Stream.of(classesToInstrument)
+                .map(c -> new ClassDefinition(c, originalBytecodes(c)))
+                .toArray(ClassDefinition[]::new);
+        inst.redefineClasses(classDefinitions);
         LOGGER.trace("All done!");
+    }
+
+    private static byte[] originalBytecodes(Class<?> c) {
+        try {
+            return Instrumenter.getClassFileInfo(c).bytecodes();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Agent.class);
